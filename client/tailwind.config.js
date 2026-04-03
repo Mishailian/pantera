1dcc217ce597afb2d2a302744f1246933bf25f70
mishailian <mishailian@github.com>
2026-04-03 07:14:26 -0700

    updated some versions react 19 and e.t.c

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [],
   theme: {
-    extend: {
-      gridTemplateRows: {
-        16: "repeat(16, minmax(0, 1fr))",
-      },
-      colors: {
-        sideBar: "var(--color-sideBar)",
-        mainBG: "var(--color-mainBG)",
-        colarC: "var(--color-colarC)",
-        blueC: "var(--color-blueC)",
-        brownC: "var(--color-brownC)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    global: true,
-    environment: "jsdom",
-    setupFiles: "./__tests__/setup",
-  },
+  server: {
+    host: true,
+    port: 5173
+  }
 });
