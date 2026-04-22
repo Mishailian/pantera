ec7236d5ad65fbd9948d6d7db4660f8dec28d147
FankiCH <117479116+FankiCH@users.noreply.github.com>
2026-04-22 16:16:00 +0500

    Update apiSlice.js

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -15,7 +15,7 @@ const buildQueryParams = (params = {}) => {
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    fullUrl: import.meta.env.VITE_API_URL || "http://localhost/api/v1",
+    baseUrl: import.meta.env.VITE_API_URL || "http://localhost/api/v1",
     prepareHeaders: (headers, { getState }) => {
       const token = getState()?.auth?.token;
       if (token) {
